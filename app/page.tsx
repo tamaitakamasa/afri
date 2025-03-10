73d700c6dcf54856c378e5d4ea826f6d21a2e2a9
tamai <tamai@shimatoworks.jp>
2025-03-10 18:27:12 +0900

    feat: 動画プレーヤーのスタイルを改善
    - HomeコンポーネントにVideoPlayerをラップするdivを追加し、位置を調整
    - VideoPlayerコンポーネントにclipPathスタイルを追加
    - 動画プレーヤーのプロパティを整理し、playsinlineを追加
    - 不要なコメントを削除し、コードをクリーンアップ

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,20 @@
 
 import VideoPlayer from "@/components/common/video-player";
 
-
-
-
 export default function Home() {
   return (
     <div className="">
-      <VideoPlayer url="https://www.youtube.com/watch?v=4MgLFWefpbk" />
+      <div className="absolute inset-0 h-full w-full">
+        <VideoPlayer url="https://www.youtube.com/watch?v=4MgLFWefpbk" />
+        {/* <div
+          className="absolute inset-0 bg-white"
+          style={{
+            clipPath:
+              "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%, 0% 0%, 20% 0%, 20% 20%, 80% 20%, 80% 80%, 20% 80%, 20% 20%, 20% 0%, 0% 0%)",
+          }}
+        /> */}
+
+      </div>
     </div>
   );
 }
--- components/common/video-player.tsx
+++ components/common/video-player.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+// import { div } from "motion/react-client";
 import dynamic from "next/dynamic";
 import React from "react";
-const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
+const ReactPlayer = dynamic(() => import("react-player"), {
+  ssr: false,
+});
 
 interface VideoPlayerProps {
   url: string;
@@ -10,16 +13,20 @@ interface VideoPlayerProps {
 
 export default function VideoPlayer({ url }: VideoPlayerProps) {
   return (
-    <div className="aspect-video w-full">
-      <ReactPlayer
-        url={url}
-        width="100%"
-        height="100%"
-				muted={true}
-        controls={false}
-        playing={true}
-        light={true} // サムネイルを表示し、クリックでロード
-      />
-    </div>
+    <>
+      <div className="aspect-video w-full" style={{clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)"}}>
+        <ReactPlayer
+          url={url}
+          width="100%"
+          height="100%"
+          muted={true}
+          controls={false}
+          playing={true}
+          playsinline={true}
+          light={true} // サムネイルを表示し、クリックでロード
+        />
+        <div className="absolute top-0 border-4 bg-indigo-500 bg-clip-border p-3"></div>
+      </div>
+    </>
   );
 }
