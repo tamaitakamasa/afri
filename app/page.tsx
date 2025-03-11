811ead2138407daf4203b0732491764846a67f4b
tamai <tamai@shimatoworks.jp>
2025-03-11 14:15:51 +0900

    feat: runtimeをエッジに設定
    - layout.tsx: runtimeをエッジに設定
    - page.tsx: runtimeをエッジに設定

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
+export const runtime = "edge";
+
 export default function Home() {
   return (
     <div className="relative h-dvh">
