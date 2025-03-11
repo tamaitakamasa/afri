999926689c3af6df113c8e8d59ad41cca3140b3f
tamai <tamai@shimatoworks.jp>
2025-03-11 18:39:30 +0900

    feat: ノートリストコンポーネントのレイアウトを更新
    ノートリストコンポーネントのスタイルを改善し、チームセクションを追加しました。各投稿の画像をリンクで囲み、より良いユーザー体験を提供します。

--- components/note/note-list.tsx
+++ components/note/note-list.tsx
@@ -8,29 +8,42 @@ export default async function NoteList() {
   // console.log(posts);
 
   return (
-    <div>
-      {posts.map((post) => (
-        <div key={post.id}>
-          <figure className="aspect-square max-w-[400px] relative">
-            <Image
-              src={post.eyecatch}
-              alt={post.name}
-              fill
-							sizes="400px"
-              style={{ objectFit: "cover" }}
-            />
-          </figure>
-          <h2>
-            <a
-              href={post.noteUrl}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {post.name}
-            </a>
-          </h2>
+    <div className="container mx-auto px-4 py-8">
+      <div className="relative flex">
+        <div className="min-w-1/4 sticky top-0 h-fit py-24">
+          <h2 className="text-2xl font-semibold">TEAMS</h2>
         </div>
-      ))}
+        <div className="flex w-full flex-col gap-4">
+          {posts.map((post) => (
+            <div key={post.id}>
+              <figure className="relative aspect-square max-w-[400px]">
+                <a
+                  href={post.noteUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    src={post.eyecatch}
+                    alt={post.name}
+                    fill
+                    sizes="400px"
+                    style={{ objectFit: "cover" }}
+                  />
+                </a>
+              </figure>
+              <h2>
+                <a
+                  href={post.noteUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {post.name}
+                </a>
+              </h2>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
