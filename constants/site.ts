2d3ca2f471fbb85da62847f1c2b0d21e660d8a39
tamai <tamai@shimatoworks.jp>
2025-03-12 10:58:53 +0900

    feat: サイトタイトルと説明を定数から設定
    - layout.tsx: サイトタイトルと説明を定数 SITE_TITLE と SITE_DESCRIPTION に更新
    - site.ts: サイトタイトルと説明を「Awaji Food Research Institute」と「淡路島（兵庫県）で、「食」をテーマに研究・活動。」に設定

--- constants/site.ts
+++ constants/site.ts
@@ -2,8 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_URL = "";
-export const SITE_TITLE = "";
-export const SITE_DESCRIPTION = "";
+export const SITE_TITLE = "Awaji Food Research Institute";
+export const SITE_DESCRIPTION = "淡路島（兵庫県）で、「食」をテーマに研究・活動。";
 export const NOTE_URL = "https://note.com/foodtrail";
 export const NOTE_API_BASE_URL = 'https://note.com/api/v2/creators/foodtrail/';
 export const NAVI_ITEMS = [
