81653f0e04c4ea0ea38eb69ab9cd063cfeba44a6
tamai <tamai@shimatoworks.jp>
2025-03-12 10:36:27 +0900

    feat: フッターコンポーネントのレイアウトを改善し、リンクを更新
    - ボタンをリンクに変更し、ナビゲーションアイテムの表示を改善
    - ノートへのリンクを追加
    - フッターのスタイルを調整し、余白を増加

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,30 +1,33 @@
-import { NAVI_ITEMS } from "@/constants/site";
-import { Button } from "../ui/button";
+import { NAVI_ITEMS, NOTE_URL } from "@/constants/site";
 import Link from "next/link";
 import { Separator } from "../ui/separator";
 
 export default function Footer() {
   return (
-    <div className="bg-gray-900 p-4 text-white">
-      <div className="container mx-auto">
-        <div className="flex gap-4">
+    <div className="bg-gray-900 py-24 text-white">
+      <div className="container mx-auto px-4">
+        <div className="flex gap-8">
           {NAVI_ITEMS.map((item, index) => (
-            <Button
-              asChild
+            <Link
               key={index}
-              variant="ghost"
-              className="block py-2"
+              href={item.href}
+              className="font-bold uppercase"
             >
-              <Link href={item.href} className="font-bold uppercase">
-                {item.label}
-              </Link>
-            </Button>
+              {item.label}
+            </Link>
           ))}
         </div>
-				<Separator className="my-12" />
+        <Separator className="my-12" />
         <div className="flex items-center justify-between">
-          <div>© 2021 Company Name</div>
-          <div>Privacy Policy</div>
+          <div>
+            <Link href={NOTE_URL} target="_blank">
+              note
+            </Link>
+          </div>
+          <div className="flex gap-4 text-sm font-bold">
+            <div>Privacy Policy</div>
+						<div className="text-gray-500">© 2021 Company Name</div>
+          </div>
         </div>
       </div>
     </div>
