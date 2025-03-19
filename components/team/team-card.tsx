e0d6e8190426d7bc9e97987394222b45d3e2fe0a
tamai <tamai@shimatoworks.jp>
2025-03-20 08:17:48 +0900

    feat: チームメンバーの画像を追加
    - TeamCardコンポーネントに画像プロパティを追加し、画像がある場合は表示するように変更
    - TeamListコンポーネントで各チームメンバーに画像を設定

--- components/team/team-card.tsx
+++ components/team/team-card.tsx
@@ -1,9 +1,12 @@
 // components/team/team-card.tsx
+import Image, { StaticImageData } from "next/image";
+
 interface TeamCardProps {
   name: string;
   en: string;
   company?: string;
   description: string;
+  image?: StaticImageData;
 }
 
 export default function TeamCard({
@@ -11,11 +14,23 @@ export default function TeamCard({
   en,
   company,
   description,
+  image,
 }: TeamCardProps) {
   return (
     <div>
       <div className="flex gap-4">
-        <div className="aspect-square w-full max-w-[400px] bg-gray-200"></div>
+        <div className="aspect-square w-full max-w-[400px] relative">
+          {image ? (
+            <Image
+              src={image}
+              alt={name}
+              fill
+              className="object-cover"
+            />
+          ) : (
+            <div className="w-full h-full bg-gray-200"></div>
+          )}
+        </div>
         <div className="flex flex-col justify-center gap-4 p-4">
           <hgroup className="flex flex-col gap-8">
             {company && (
